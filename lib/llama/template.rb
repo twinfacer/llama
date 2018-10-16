6de781827e6e5f2bb0b4a0c9bb836f3f5a9dc752
Sergey Ivannikov <twinfacer@gmail.com>
2018-10-16 17:21:32 +0300

    template now can be direct git url

--- lib/llama/template.rb
+++ lib/llama/template.rb
@@ -1,9 +1,15 @@
 module Llama
   class Template
-    attr_accessor :template_name
+    attr_accessor :template_name, :repo_path
 
-    def initialize(template_name)
-      @template_name = template_name
+    def initialize(template)
+      if template.include?('.git')
+        @template_name = template.match(/\/(.+)\.git$/)[1]
+        @repo_path = template
+      else
+        @template_name = template.split('/').last
+        @repo_path = "git@github.com:#{template_name}.git"
+      end
     end
 
     def render!(output_path, project_name)
@@ -19,13 +25,8 @@ module Llama
       `git clone #{repo_path} #{template_path} 2>&1 1>/dev/null`
     end
 
-    def repo_path
-      "git@github.com:#{template_name}.git"
-    end
-
     def template_path
-      File.expand_path("~/.llama/templates/#{template_name.split('/').last}")
+      File.expand_path("~/.llama/templates/#{template_name}")
     end
-
   end
 end
